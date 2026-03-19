55287edc06bd0ef2951b386ebfc21d1e2cf7f872
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-03-19 11:11:21 -0600

    Created graph compilation script in 'graph.py'
    On branch graph_implementation
    Changes to be committed:
          new file:   graph.py

---
+++ src/graph/graph.py
@@ -0,0 +1,32 @@
+"""
+graph.py
+
+Here we wire all our nodes into a graph
+
+J. A. Moreno
+2026
+"""
+
+
+import nodes
+from state import JobPrepState
+from langgraph.graph import StateGraph, START, END
+
+# Assemble the graph
+workflow = StateGraph(JobPrepState)
+
+# Add nodes
+workflow.add_node("scrap_job_posting", nodes.scrap_job_posting)
+workflow.add_node("distill_query", nodes.distill_search_query)
+workflow.add_node("search_user_data", nodes.search_user_db)
+workflow.add_node("draft_answer", nodes.draft_answer)
+
+# Declare edges
+workflow.add_edge(START, "scrap_job_posting")
+workflow.add_edge("scrap_job_posting", "distill_query")
+workflow.add_edge("distill_query", "search_user_data")
+workflow.add_edge("search_user_data", "draft_answer")
+workflow.add_edge("draft_answer", END)
+
+# Compile the graph
+graph = workflow.compile()
