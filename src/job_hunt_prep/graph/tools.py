3b1b7b26b1ab7d0dd1be8d062e7cd233fcaab09b
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-03-24 16:25:42 -0600

    Added web scrapping and user search as tools

--- src/job_hunt_prep/graph/tools.py
+++ src/job_hunt_prep/graph/tools.py
@@ -9,6 +9,7 @@ J. A. Moreno
 
 from typing import List, Tuple
 from langchain.tools import tool
+from langchain_community.document_loaders import SeleniumURLLoader
 from langchain_core.documents import Document
 
 from . import llm_setup
@@ -20,12 +21,16 @@ retriever = llm_setup.Retriever().retriever
 @tool
 def search_user_db(query: str, k: int = 5) -> Tuple[str, List[Document]]:
     """
-    Semantically searches a database containing user's personal information
+    Semantically searches a vector store containing user's personal information
     matching the query.
 
     Args:
         query: Search terms to look for
         k: The maximum amount of desired results
+
+    Returns:
+        serialized: Serialized documents from the vector store
+        retrieved_docs: Retrieved Documents from the vector store
     """
     try:
         retrieved_docs = retriever.invoke(str(query), k=k)
@@ -43,3 +48,36 @@ def search_user_db(query: str, k: int = 5) -> Tuple[str, List[Document]]:
     except Exception:
         Exception("Search turned no elemments")
         return ("No elements found", [])
+
+
+@tool
+def scrap_job_posting(job_post_url: str) -> Tuple[str, List[Document]]:
+    """
+    Scraps the job post located in job_post_url and loads
+    as a list of Documents using SeleniumURLLoader.
+
+    Args:
+        job_post_url: A valid URL containing a job post
+
+    Returns:
+        serialized: Serialized documents from the job post
+        retrieved_post: Retrieved Documents from the job post
+
+    """
+    # Create the url loader
+    loader = SeleniumURLLoader(
+        [job_post_url],
+    )
+    # Load data
+    retrieved_post = loader.load()
+    # Serialize
+    serialized = "\n\n".join(
+        (
+            f"Source: {doc.metadata.get('source', 'Unknown')}"
+            f"\n\nTitle: {doc.metadata.get('title', 'Unknown')}"
+            + f"\n\nContent: {doc.page_content}"  # Specific to SeleniumURLLoader
+        )
+        for doc in retrieved_post
+    )
+    # Return serialized and raw docs
+    return (serialized, retrieved_post)
