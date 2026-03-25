3d508a07b3b6a64b77a1492ec75ea7479d4c6920
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-03-24 19:07:21 -0600

    Modified tools so that they update the graph's state

--- src/job_hunt_prep/graph/tools.py
+++ src/job_hunt_prep/graph/tools.py
@@ -7,10 +7,10 @@ J. A. Moreno
 2026
 """
 
-from typing import List, Tuple
-from langchain.tools import tool
+from langchain.tools import tool, ToolRuntime
 from langchain_community.document_loaders import SeleniumURLLoader
-from langchain_core.documents import Document
+from langchain.messages import ToolMessage
+from langgraph.types import Command
 
 from . import llm_setup
 
@@ -19,21 +19,19 @@ retriever = llm_setup.Retriever().retriever
 
 # Define tools
 @tool
-def search_user_db(query: str, k: int = 5) -> Tuple[str, List[Document]]:
+def search_user_db(runtime: ToolRuntime) -> Command:
     """
     Semantically searches a vector store containing user's personal information
     matching the query.
 
-    Args:
-        query: Search terms to look for
-        k: The maximum amount of desired results
-
-    Returns:
-        serialized: Serialized documents from the vector store
-        retrieved_docs: Retrieved Documents from the vector store
+    Sets:
+        serialized_documents: Serialized documents from the vector store
+        retrieved_documents: Retrieved Documents from the vector store
     """
+
+    query = runtime.state["distilled_query"]
     try:
-        retrieved_docs = retriever.invoke(str(query), k=k)
+        retrieved_docs = retriever.invoke(str(query), k=5)
         # Serialize documents for the model
         serialized = "\n\n".join(
             (
@@ -43,27 +41,47 @@ def search_user_db(query: str, k: int = 5) -> Tuple[str, List[Document]]:
             for doc in retrieved_docs
         )
         # Return serialized and raw documents
-        return (serialized, retrieved_docs)
+        return Command(
+                update={
+                    "serialized_documents": serialized,
+                    "retrieved_documents": retrieved_docs,
+                    "messages": [
+                    ToolMessage(
+                        content="Retrieved docs!",
+                        tool_call_id=runtime.tool_call_id
+                    )
+                    ]
+                }
+        )
 
     except Exception:
         Exception("Search turned no elemments")
-        return ("No elements found", [])
+        return Command(
+                update={
+                    "serialized_documents": None,
+                    "retrieved_documents": None,
+                    "messages": [
+                    ToolMessage(
+                        content="Failed to retrieve docs",
+                        tool_call_id=runtime.tool_call_id
+                    )
+                    ]
+                }
+        )
 
 
 @tool
-def scrap_job_posting(job_post_url: str) -> Tuple[str, List[Document]]:
+def scrap_job_posting(runtime: ToolRuntime) -> Command:
     """
     Scraps the job post located in job_post_url and loads
     as a list of Documents using SeleniumURLLoader.
 
-    Args:
-        job_post_url: A valid URL containing a job post
-
-    Returns:
-        serialized: Serialized documents from the job post
-        retrieved_post: Retrieved Documents from the job post
+    Sets the state as follows:
+        serialized_job_post: Serialized documents from the job post
+        job_post_documents: Retrieved Documents from the job post
 
     """
+    job_post_url = runtime.state["job_post_url"]
     # Create the url loader
     loader = SeleniumURLLoader(
         [job_post_url],
@@ -80,4 +98,15 @@ def scrap_job_posting(job_post_url: str) -> Tuple[str, List[Document]]:
         for doc in retrieved_post
     )
     # Return serialized and raw docs
-    return (serialized, retrieved_post)
+    return Command(
+        update={
+        "serialized_job_post": serialized,
+        "job_post_documents": retrieved_post,
+        "messages": [
+            ToolMessage(
+                    content="Failed to retrieve docs",
+                    tool_call_id=runtime.tool_call_id
+                )
+            ]
+        }
+    )
