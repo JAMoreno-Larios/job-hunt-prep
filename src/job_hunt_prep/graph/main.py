32ad35e4b3a46073f0e15667cb530d9b26b2578e
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-03-25 20:10:40 -0600

    Added more cases

--- src/job_hunt_prep/graph/main.py
+++ src/job_hunt_prep/graph/main.py
@@ -17,8 +17,6 @@ This is the job post:https://recruiting2.ultipro.com/INF1019IRINC/JobBoard/17a8d
 They are asking: We are looking for people who are personally and/or
 professionally passionate about AI. Please briefly explain how you have
 put it to work for you in either or both areas of your life.
-
-What's the best way to answer?
     """.strip()
 
     #Initialize graph
@@ -32,10 +30,22 @@ What's the best way to answer?
             print(msg.content, end="", flush=True)
     
     user_query = """
-This is the job post:https://recruiting2.ultipro.com/INF1019IRINC/JobBoard/17a8d008-9efe-4e51-8460-47ee205d5229/OpportunityDetail?opportunityId=70528dc0-415e-4c8d-970f-46cf2a253bd0
 They are asking: What are your three main motivators for this position?
 
-What's the best way to answer?
+Additional instructions: Keep your answer below 500 characters.
+    """.strip()
+
+    for part in graph.run_agent(user_query):
+        if part["type"] == "messages":
+            # MessagesStreamPart — (message_chunk, metadata) from LLM calls
+            msg, metadata = part["data"]
+            print(msg.content, end="", flush=True)
+    
+    user_query = """
+What was a challenge you found during your past experience and how did you solved it?
+
+User insructions: Keep your answer below 250 characters, avoid technical jargon and use references
+no older than 5 years.
     """.strip()
 
     for part in graph.run_agent(user_query):
