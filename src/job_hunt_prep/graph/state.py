7187490380cba98d8e7b807e46d79ce60b679b02
José Agustín Moreno Larios <agustin90m@gmail.com>
2026-03-25 20:08:43 -0600

    Fixed spacing

--- src/job_hunt_prep/graph/state.py
+++ src/job_hunt_prep/graph/state.py
@@ -6,7 +6,6 @@ J. A. Moreno
 2026
 """
 
-
 from langchain_core.documents import Document
 from langgraph.graph import MessagesState
 
@@ -16,17 +15,18 @@ class InputState(MessagesState):
     We take a natural-language user input that contains the
     URL for job_post_url and a job related user_query.
     """
+
     raw_query: str
 
 
 class OutputState(MessagesState):
     draft_response: str | None
 
+
 class JobPrepState(MessagesState):
-    
     # Raw user query
     raw_query: str
-    
+
     # Parsed query
     user_query: str | None
     user_instructions: str | None
@@ -36,7 +36,7 @@ class JobPrepState(MessagesState):
     retrieved_documents: list[Document] | None  # Raw docs
     serialized_documents: str | None  # Serialized content
     job_post_documents: list[Document] | None
-    serialized_job_post: str | None # From web page
+    serialized_job_post: str | None  # From web page
 
     # Generated content
     distilled_query: str | None
